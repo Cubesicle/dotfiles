7ea542eb6bb37320e811fe290fa42a8d7bb27653
Cubesicle <ottboonpirom@gmail.com>
2022-11-21 23:10:57 -0600

    Remove unused import

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -10,7 +10,6 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 
 local naughty = require("naughty")
-local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
 require("awful.hotkeys_popup.keys")
