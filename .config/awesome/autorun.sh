24780055097127ead7968a8cbdb2c1c2825710cd
Cubesicle <ottboonpirom@gmail.com>
2022-11-28 14:44:22 -0600

    Rewrite and move startup script

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+run() {
+  if ! pgrep -f "$1" ;
+  then
+    "$@"&
+  fi
+}
+
+dbus-update-activation-environment --all
+gnome-keyring-daemon --start --components=secrets
+
+nitrogen --restore
+
+run gentoo-pipewire-launcher
+run picom --experimental-backends
+run nm-applet
+run udiskie -t
